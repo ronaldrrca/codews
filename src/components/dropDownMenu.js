6050f832c557b8bdcee61a0722ea6f4415c43d01
Ronald Rodriguez Campillo <ronaldrrca@gmail.com>
2022-05-17 12:40:10 -0500

    Estableciendo los estilos del elemento activo del menú, mediante NavLink de react-router

--- src/components/dropDownMenu.js
+++ src/components/dropDownMenu.js
@@ -1,14 +1,14 @@
 import React from "react";
 import '../style-sheets/dropDownMenu.css'
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function DropDownMenu() {
     return(
-        <nav id="dropDownMenu">
-            <Link to="/home" className="dropDownMenu_item">INICIO</Link>
-            <Link to="/proyectos" className="dropDownMenu_item">PROYECTOS</Link>
-            <Link to="/contactanos" className="dropDownMenu_item">CONTACTANOS</Link>
-            <Link to="/blog" className="dropDownMenu_item">BLOG</Link>
+        <nav id="dropDownMenu">{/*NavLink permite establecer estilos al elemento ativo del menú */}
+            <NavLink to="/home" className={(navData)=>navData.isActive ? "active dropDownMenu_item" : "dropDownMenu_item" }>INICIO</NavLink>
+            <NavLink to="/proyectos" className={(navData)=>navData.isActive ? "active dropDownMenu_item" : "dropDownMenu_item" }>PROYECTOS</NavLink>
+            <NavLink to="/contactanos" className={(navData)=>navData.isActive ? "active dropDownMenu_item" : "dropDownMenu_item" }>CONTACTANOS</NavLink>
+            <NavLink to="/blog" className={(navData)=>navData.isActive ? "active dropDownMenu_item" : "dropDownMenu_item" }>BLOG</NavLink>
         </nav>
     )
 }
