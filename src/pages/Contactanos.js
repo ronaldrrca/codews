66d89bcdbe1fad1dc2be5a8c688093100ed621bf
Ronald Rodriguez Campillo <ronaldrrca@gmail.com>
2022-05-18 18:27:04 -0500

    Agregando el fronend de la página de contactos

--- src/pages/Contactanos.js
+++ src/pages/Contactanos.js
@@ -1,9 +1,37 @@
 import React from "react";
+import '../style-sheets/contactanos.css'
 
 function Contactanos() {
     return(
          <main>
-            <h1>Contactanos</h1>
+            <h1 className="pageTitle">Contáctanos</h1>
+            <div id="contactWrapContainer">
+                <div id="contactInfo">
+                    <p>Solicita una propuesta</p>
+                    <ul>
+                        <li>Páginas web</li>
+                        <li>Sitios web</li>
+                        <li>Landing pages</li>
+                        <li>Aplicaciones web</li>
+                    </ul>
+                    <p id="noCost">Sin costo ni compromisos</p>
+                    <p id="conditions">(Aplican condiciones)</p>
+                </div>
+                <form>
+                    <label>Nombre*</label>
+                    <input type='text' required autoFocus></input>
+                    <label>Email*</label>
+                    <input type='email' required></input>
+                    <label>Teléfono</label>
+                    <input type='text' placeholder="Opcional"></input>
+                    <label>Mensaje*</label>
+                    <textarea type='text' required placeholder="Escriba aquí su mensaje"></textarea>
+                    <p id="required">* Campos requeridos</p>
+                    <input type='submit' value='Enviar'></input>
+                </form>
+                
+            </div>
+            
         </main>
      
     )
